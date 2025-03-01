1f238cf8844deec323de79916c76c715bfef541d
Ricardo_Cossa <ricardoccossa@gmail.com>
2025-03-01 19:08:31 +0200

    loop com for

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 alert(`Boas-vindas ao jogo do Número Secreto`)
-let numeroSecreto = 30;
-let chute = prompt (`Escolha um número entre 1 e 30`)
+let numeroSecreto = 30
+let chute
+//let tentativas = 1;
+// enquanto chute não for igual ao n.s
+for(let tentativas = 1; chute != numeroSecreto; tentativas++) {
+    chute = prompt (`Escolha um número entre 1 e 30`)
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! você descobriu o número secreto `)
-} else {
-    alert('Você errou :(')
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! você descobriu o número secreto com ${tentativas} tentativas`)
+    } else if(numeroSecreto < chute ) {
+        alert(`O número secreto é menor que ${chute}`)
+    }else {
+        alert(`O número secreto é maior que ${chute}`)
+    }
 }
+
+
